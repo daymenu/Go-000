7843954873c56879fa1a3e85ba1b180c5b1f2c9a
hanjian <hanjian2018@126.com>
2020-12-08 22:25:17 +0800

    week03

--- Week03/work/main.go
+++ Week03/work/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -10,7 +12,7 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-var errg errgroup.Group
+var serverErrGroup errgroup.Group
 
 // 基于 errgroup 实现多个 http server 的启动和关闭 ，以及 linux signal 信号的注册和处理，要保证能够 一个退出，全部注销退出。
 func main() {
@@ -19,14 +21,44 @@ func main() {
 
 	ctx, cancel := context.WithCancel(context.Background())
 
-	fmt.Println(ctx, cancel)
+	serverErrGroup.Go(func() error {
+		return gatewayServer(ctx)
+	})
+
+	serverErrGroup.Go(func() error {
+		return appServer(ctx)
+	})
+
+	if err := serverErrGroup.Wait(); err != nil {
+		log.Fatal(err)
+	}
+
+	defer cancel()
+	fmt.Println("server is start")
+
+	select {
+	case <-quit:
+		fmt.Println("")
+	}
 }
 
-func shoutdown(quit <-chan os.Signal) {
-	switch <-quit {
-	case syscall.SIGINT, syscall.SIGTERM:
-		fmt.Println("退出")
-	default:
-		fmt.Println("其他信号")
+// 一个app服务
+func appServer(ctx context.Context) error {
+	srv := &http.Server{
+		Addr:    ":80",
+		Handler: nil,
 	}
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		return err
+	}
+	// err := fmt.Errorf("app")
+	// return fmt.Errorf("server error: %w", err)
+	return nil
+}
+
+// 一个网关服务
+func gatewayServer(ctx context.Context) error {
+	// err := fmt.Errorf("gateway")
+	// return fmt.Errorf("server error: %w", err)
+	return nil
 }

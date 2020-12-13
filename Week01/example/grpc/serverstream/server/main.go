44683308885db96131cf007ea21b83ddd3446b6c
hanjian <hanjian2018@126.com>
2020-12-14 00:36:54 +0800

    服务端流式grpc

--- Week01/example/grpc/serverstream/server/main.go
+++ Week01/example/grpc/serverstream/server/main.go
@@ -2,14 +2,39 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"math"
+	"net"
 
 	"github.com/daymenu/Go-000/Week01/example/grpc/serverstream/model"
+	"google.golang.org/grpc"
 )
 
 type circularService struct {
 	*model.UnimplementedCircularServiceServer
 }
 
+func (c *circularService) Area(request *model.AreaRequest, stream model.CircularService_AreaServer) error {
+	resp := new(model.AreaResponse)
+	resp.Code = 200
+	radius := request.Circular.Radius
+	resp.Area = radius * radius * math.Pi
+	stream.Send(resp)
+	stream.Send(resp)
+	return nil
+}
+
 func main() {
-	fmt.Println("hello")
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":10000"))
+	if err != nil {
+		log.Fatalf("listen err：%v", err)
+	}
+
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	log.Println("register area server")
+	model.RegisterCircularServiceServer(grpcServer, new(circularService))
+	log.Println("server is serve")
+	grpcServer.Serve(lis)
 }
